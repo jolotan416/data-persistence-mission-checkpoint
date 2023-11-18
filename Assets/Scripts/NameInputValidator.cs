44c01c5e23b2198bc4e9de0064468aabdc0d625d
Jolo Tan <jolotan416@gmail.com>
2023-11-19 00:17:41 +0100

    Add scene for menu

---
+++ Assets/Scripts/NameInputValidator.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class NameInputValidator : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI errorText;
+
+    [SerializeField]
+    private TextMeshProUGUI nameText;
+
+    public void Start()
+    {
+        errorText.enabled = false;
+    }
+
+    public void ValidateName()
+    {
+        string trimmedName = nameText.text.Trim();
+        if (trimmedName.Length <= 1)
+        {
+            errorText.enabled = true;
+        } else
+        {
+            // Main scene is set at index 1
+            SceneManager.LoadScene(1);
+        }
+    }
+}
